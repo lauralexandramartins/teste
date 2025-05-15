3a7e99353392c8aaef77a73dc681cf5a60bb6b35
lauralexandramartins <lauralexandra146@gmail.com>
2025-05-15 11:15:32 -0300

    TESTE

--- obj/Debug/net9.0/Projeto.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5101debc6ddbf1990b3ac0eb4b2c4d7ce93475fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
